24179d9ed9dcfec73c8f40f59d6e1cb8a7fb741e
wulongfei.wlf <wulongfei.wlf@bytedance.com>
2023-11-06 16:23:11 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,29 @@
+import os
 import torch
+import torch.distributed as dist
+from torch.multiprocessing import Process
 
-print("hello torch{}".format(torch.__version__))
+def run(rank, size):
+    """ Distributed function to be implemented later. """
+    group = dist.new_group([i for i in range(size)])
+    tensor = torch.ones(1)
+    dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
+    print('Rank ', rank, ' has data ', tensor[0])
+
+def init_process(rank, size, fn, backend='gloo'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
+
+if __name__ == "__main__":
+    size = 2   # 使用两个进程
+    processes = []
+    for rank in range(size):
+        p = Process(target=init_process, args=(rank, size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
