4a387115426f91b9c124a8d8addc18981852803b
wulongfei.wlf <wulongfei.wlf@bytedance.com>
2023-11-06 18:05:01 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,29 +1,43 @@
-import os
 import torch
 import torch.distributed as dist
-from torch.multiprocessing import Process
+import socket
+from multiprocessing import Process
 
-def run(rank, size):
-    """ Distributed function to be implemented later. """
-    group = dist.new_group([i for i in range(size)])
-    tensor = torch.ones(1)
-    dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
+def find_free_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 0))
+        return s.getsockname()[1]
+
+def main(rank, world, port):
+    # Specify the initialization method and the port
+    init_method = 'tcp://localhost:' + str(port)
+
+    # Initialize the distributed environment.
+    dist.init_process_group(
+        backend='gloo',
+        init_method=init_method,
+        rank=rank,
+        world_size=world
+    )
+
+    # Create a tensor with value `rank`.
+    tensor = torch.ones(1) * rank
+
+    # AllReduce operation.
+    dist.all_reduce(tensor, op=dist.ReduceOp.SUM)
+
     print('Rank ', rank, ' has data ', tensor[0])
 
-def init_process(rank, size, fn, backend='gloo'):
-    """ Initialize the distributed environment. """
-    os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = '29500'
-    dist.init_process_group(backend, rank=rank, world_size=size)
-    fn(rank, size)
-
-if __name__ == "__main__":
-    size = 2   # 使用两个进程
+def run_demo(demo_fn, world_size):
+    port = find_free_port()
     processes = []
-    for rank in range(size):
-        p = Process(target=init_process, args=(rank, size, run))
+    for rank in range(world_size):
+        p = Process(target=demo_fn, args=(rank, world_size, port))
         p.start()
         processes.append(p)
 
     for p in processes:
         p.join()
+
+# For example, if you have 4 GPUs.
+run_demo(main, 1)
