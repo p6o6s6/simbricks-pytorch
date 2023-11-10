b5a416c70f48c3641481c9f9764135f0fd7f71b1
wulongfei.wlf <wulongfei.wlf@bytedance.com>
2023-11-10 15:33:29 +0800

    Update tcpstore.py

--- tcpstore.py
+++ tcpstore.py
@@ -5,6 +5,7 @@ def run():
     # 使用IPv4地址，例如'127.0.0.1'代表localhost
     # 选择一个未被使用的端口，例如'23456'
     # 设置is_master为True，表示这是主节点
+    os.environ["GLOO_SOCKET_IFNAME"] = "^lo,docker0"
     tcp_store = dist.TCPStore('127.0.0.1', 23456, is_master=True)
 
     # 你可以使用TCPStore对象来设置和获取键值对
