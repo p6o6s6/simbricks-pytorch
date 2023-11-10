533d9ba28c10fe9a2966dcd245794929e3f1ea34
wulongfei.wlf <wulongfei.wlf@bytedance.com>
2023-11-10 15:06:26 +0800

    Update tcpstore.py

--- tcpstore.py
+++ tcpstore.py
@@ -2,10 +2,10 @@ import torch.distributed as dist
 
 def run():
     # 创建一个TCPStore对象
-    # 使用IPv6地址，例如'::1'代表localhost
+    # 使用IPv4地址，例如'127.0.0.1'代表localhost
     # 选择一个未被使用的端口，例如'23456'
     # 设置is_master为True，表示这是主节点
-    tcp_store = dist.TCPStore('::1', 23456, is_master=True)
+    tcp_store = dist.TCPStore('127.0.0.1', 23456, is_master=True)
 
     # 你可以使用TCPStore对象来设置和获取键值对
     tcp_store.set("key", "value")
